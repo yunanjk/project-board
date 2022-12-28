f13785da59c2da5fac5140661f8614cfae03dfb7
yunanjk <lyra082172@naver.com>
2022-12-28 09:31:06 +0900

    #9 - 도메인 클래스 설계
    게시글, 댓글 설계

--- project-board/src/main/java/com/koreait/projectboard/domain/ArticleComment.java
+++ project-board/src/main/java/com/koreait/projectboard/domain/ArticleComment.java
@@ -4,10 +4,10 @@ import java.time.LocalDateTime;
 
 public class ArticleComment {
     private long id;
-    private long article_id;
-    private String content;
-    private LocalDateTime createdAt;
-    private String createdBy;
-    private LocalDateTime modifiedAt;
-    private String modifiedBy;
+    private Article article; // 게시글(id)
+    private String content; // 본문
+    private LocalDateTime createdAt; // 생성일시
+    private String createdBy; // 생성자
+    private LocalDateTime modifiedAt; // 수정일시
+    private String modifiedBy; // 수정자
 }
